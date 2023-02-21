c4f1d682e3a4c4442823ed431122bdb98651ad04
uroboros-d <125786692+uroboros-d@users.noreply.github.com>
2023-02-21 23:21:45 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd script 				        #зайти в папку
-mkdir dir_1 dir_2 dir_3			      #создать 3 папки
-cd dir_1				   					#зайти в любую папку
+mkdir dir_1 dir_2 dir_3			        #создать 3 папки
+cd dir_1				   	#зайти в любую папку
 touch 11.txt 22.txt 33.txt 44.json 55.json 	#создать 5 файлов
 mkdir fold1 fold2 fold3					    #создать 3 папки
 ls -la					   					#вывести список содержимого папки
